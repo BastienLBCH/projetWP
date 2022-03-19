e16a4e159d05721337adb3b11be6e8a10179d816
BastienLBCH <bastien.labouche@live.fr>
2022-03-19 23:52:01 +0100

    debug

--- numeroutile_module/models/model_numeroutile.php
+++ numeroutile_module/models/model_numeroutile.php
@@ -1,14 +1,14 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=livret_accueil;', 'marie.blanc', 'cjbS8l1aiGTBXLEx');
+$db_numeroutile = new PDO('mysql:host=localhost;dbname=livret_accueil;', 'marie.blanc', 'cjbS8l1aiGTBXLEx');
 
-function db_create_numeroutile($data){
-    global $db;
+function db_numeroutile_create_numeroutile($data){
+    global $db_numeroutile;
 
     echo "toto";
 
     $sql = "INSERT INTO numeroutile (nomcontact, descp, tel) VALUES (:_nomcontact, :_descp, :_tel)";
     
-    $query = $db->prepare($sql);
+    $query = $db_numeroutile->prepare($sql);
 
     $query->execute(array(
         "_nomcontact" => $data["nomcontact"],
@@ -18,12 +18,12 @@ function db_create_numeroutile($data){
 }
 
 
-function db_get_numeroutile($numeroutile_id) {
-    global $db;
+function db_numeroutile_get_numeroutile($numeroutile_id) {
+    global $db_numeroutile;
 
     $sql = "SELECT * FROM numeroutile WHERE id=:_id";
 
-    $query = $db->prepare($sql);
+    $query = $db_numeroutile->prepare($sql);
     $query->execute(array(
         "_id" => $numeroutile_id
     ));
@@ -31,11 +31,11 @@ function db_get_numeroutile($numeroutile_id) {
     return $query->fetch();
 }
 
-function db_update_numeroutile($data) {
-    global $db;
+function db_numeroutile_update_numeroutile($data) {
+    global $db_numeroutile;
 
     $sql = "UPDATE numeroutile SET nomcontact=:_nomcontact, descp=:_descp, tel=:_tel WHERE id=:_id";
-    $query = $db->prepare($sql);
+    $query = $db_numeroutile->prepare($sql);
     $query->execute(array(
         "_nomcontact" => $data["nomcontact"],
         "_descp" => $data["descp"],
@@ -46,21 +46,21 @@ function db_update_numeroutile($data) {
 }
 
 
-function db_delete_numeroutile($data) {
-    global $db;
+function db_numeroutile_delete_numeroutile($data) {
+    global $db_numeroutile;
     $sql = "DELETE FROM numeroutile WHERE id=:_id";
-    $query = $db->prepare($sql);
+    $query = $db_numeroutile->prepare($sql);
     $query->execute(array(
         "_id" => $data["id"]
     ));
 }
 
 
-function db_list_numeroutile(){
-    global $db;
+function db_numeroutile_list_numeroutile(){
+    global $db_numeroutile;
 
     $sql = "SELECT * FROM numeroutile";
-    $query = $db->prepare($sql);
+    $query = $db_numeroutile->prepare($sql);
     $query->execute();
 
     $list_numeroutile = $query->fetchAll();
@@ -73,9 +73,9 @@ die(print_r($_POST));
 
 if(isset($_POST["module"]) && isset($_POST["action"])) {
     $actions_mapping = [
-        "create" => "db_create_numeroutile",
-        "update" => "db_update_numeroutile",
-        "delete" => "db_delete_numeroutile"
+        "create" => "db_numeroutile_create_numeroutile",
+        "update" => "db_numeroutile_update_numeroutile",
+        "delete" => "db_numeroutile_delete_numeroutile"
     ];
 
     $module = $_POST["module"];

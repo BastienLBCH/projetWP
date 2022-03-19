92a4bc72cca1025fa46b103624f6d656fb1fefc0
BastienLBCH <bastien.labouche@live.fr>
2022-03-19 23:11:10 +0100

    debug

--- numeroutile_module/models/model_numeroutile.php
+++ numeroutile_module/models/model_numeroutile.php
@@ -1,92 +1,91 @@
 <?php
 $db = new PDO('mysql:host=localhost;dbname=livret_accueil;', 'marie.blanc', 'cjbS8l1aiGTBXLEx');
 
-function db_create_numeroutile($data){
-    global $db;
+// function db_create_numeroutile($data){
+//     global $db;
 
-    echo "toto";
+//     echo "toto";
 
-    $sql = "INSERT INTO numeroutile (nomcontact, descp, tel) VALUES (:_nomcontact, :_descp, :_tel)";
+//     $sql = "INSERT INTO numeroutile (nomcontact, descp, tel) VALUES (:_nomcontact, :_descp, :_tel)";
     
-    $query = $db->prepare($sql);
+//     $query = $db->prepare($sql);
 
-    $query->execute(array(
-        "_nomcontact" => $data["nomcontact"],
-        "_descp" => $data["descp"],
-        "_tel" => $data["tel"]
-    ));
-}
-
-
-function db_get_numeroutile($numeroutile_id) {
-    global $db;
-
-    $sql = "SELECT * FROM numeroutile WHERE id=:_id";
-
-    $query = $db->prepare($sql);
-    $query->execute(array(
-        "_id" => $numeroutile_id
-    ));
-
-    return $query->fetch();
-}
-
-function db_update_numeroutile($data) {
-    global $db;
-
-    $sql = "UPDATE numeroutile SET nomcontact=:_nomcontact, descp=:_descp, tel=:_tel WHERE id=:_id";
-    $query = $db->prepare($sql);
-    $query->execute(array(
-        "_nomcontact" => $data["nomcontact"],
-        "_descp" => $data["descp"],
-        "_tel" => $data["tel"],
-        "_id" => $data["id"]
-    ));
-
-}
+//     $query->execute(array(
+//         "_nomcontact" => $data["nomcontact"],
+//         "_descp" => $data["descp"],
+//         "_tel" => $data["tel"]
+//     ));
+// }
 
 
-function db_delete_numeroutile($data) {
-    global $db;
-    $sql = "DELETE FROM numeroutile WHERE id=:_id";
-    $query = $db->prepare($sql);
-    $query->execute(array(
-        "_id" => $data["id"]
-    ));
-}
+// function db_get_numeroutile($numeroutile_id) {
+//     global $db;
+
+//     $sql = "SELECT * FROM numeroutile WHERE id=:_id";
+
+//     $query = $db->prepare($sql);
+//     $query->execute(array(
+//         "_id" => $numeroutile_id
+//     ));
+
+//     return $query->fetch();
+// }
+
+// function db_update_numeroutile($data) {
+//     global $db;
+
+//     $sql = "UPDATE numeroutile SET nomcontact=:_nomcontact, descp=:_descp, tel=:_tel WHERE id=:_id";
+//     $query = $db->prepare($sql);
+//     $query->execute(array(
+//         "_nomcontact" => $data["nomcontact"],
+//         "_descp" => $data["descp"],
+//         "_tel" => $data["tel"],
+//         "_id" => $data["id"]
+//     ));
+
+// }
 
 
-function db_list_numeroutile(){
-    global $db;
-
-    $sql = "SELECT * FROM numeroutile";
-    $query = $db->prepare($sql);
-    $query->execute();
-
-    $list_numeroutile = $query->fetchAll();
-    return $list_numeroutile;
-}
+// function db_delete_numeroutile($data) {
+//     global $db;
+//     $sql = "DELETE FROM numeroutile WHERE id=:_id";
+//     $query = $db->prepare($sql);
+//     $query->execute(array(
+//         "_id" => $data["id"]
+//     ));
+// }
 
 
-print_r($_GET);
-if(isset($_GET["module"]) && isset($_GET["action"])) {
-    $actions_mapping = [
-        "create" => "db_create_numeroutile",
-        "update" => "db_update_numeroutile",
-        "delete" => "db_delete_numeroutile"
-    ];
+// function db_list_numeroutile(){
+//     global $db;
 
-    $module = $_GET["module"];
-    $action = $_GET["action"];
+//     $sql = "SELECT * FROM numeroutile";
+//     $query = $db->prepare($sql);
+//     $query->execute();
 
-    echo "<br> Hello, je suis dans le isset";
+//     $list_numeroutile = $query->fetchAll();
+//     return $list_numeroutile;
+// }
 
 
-    if( $module == "numeroutile" && array_key_exists($action, $actions_mapping) ){
-        echo "<br> Est-ce qu'il y a quelqu'un dans le action_mapping";
-        $actions_mapping[$_GET["action"]]($_GET);
-    }
-}
+// if(isset($_GET["module"]) && isset($_GET["action"])) {
+//     $actions_mapping = [
+//         "create" => "db_create_numeroutile",
+//         "update" => "db_update_numeroutile",
+//         "delete" => "db_delete_numeroutile"
+//     ];
+
+//     $module = $_GET["module"];
+//     $action = $_GET["action"];
+
+//     echo "<br> Hello, je suis dans le isset";
+
+
+//     if( $module == "numeroutile" && array_key_exists($action, $actions_mapping) ){
+//         echo "<br> Est-ce qu'il y a quelqu'un dans le action_mapping";
+//         $actions_mapping[$_GET["action"]]($_GET);
+//     }
+// }
 
 
 ?>

18df015ed7a6c9b1f4260fda9be00787f2b1423f
BastienLBCH <bastien.labouche@live.fr>
2022-03-23 12:46:50 +0100

    Update delete_hotspot.php

--- module_hotspot/views/delete_hotspot.php
+++ module_hotspot/views/delete_hotspot.php
@@ -1,5 +1,5 @@
 <?php
-    global $ACTIONS_URL_WIFI;
+    global $ACTIONS_URL_HOTSPOT;
 
     ob_start();
 ?>
@@ -9,26 +9,26 @@
 
         // Crée un formulaire qui sera envoyé via une requête JS
         var form_data = new FormData();
-        form_data.append("module", "wifi");
+        form_data.append("module", "hotspot");
         form_data.append("action", "delete");
-        form_data.append("id", "<?= $_GET["wifi_id"] ?>");
+        form_data.append("id", "<?= $_GET["hotspot_id"] ?>");
 
-        let placewifiDiv = document.getElementById("placewifiDiv");
+        let placewifiDiv = document.getElementById("placeHotspotDiv");
 
         // Crée une requête qui enverra le formulaire
         var request = new XMLHttpRequest();
-        request.open("POST", "<?= $ACTIONS_URL_WIFI["database"] ?>");
+        request.open("POST", "<?= $ACTIONS_URL_HOTSPOT["database"] ?>");
         request.send(form_data);
 
         request.onreadystatechange = function() {
             if(request.readyState === 4){
-                // Renvoie l'utilisateur vers la liste d'wifi
-                document.location.href = "<?= $ACTIONS_URL_WIFI["list"] ?>";
+                // Renvoie l'utilisateur vers la liste de hotspot
+                document.location.href = "<?= $ACTIONS_URL_HOTSPOT["list"] ?>";
             }
         };
     });
 </script>
 
 <?php
-    $detele_wifi_view = ob_get_clean();
+    $detele_hotspot_view = ob_get_clean();
 ?>
